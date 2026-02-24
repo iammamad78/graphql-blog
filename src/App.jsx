927cf1f200aca20bb7bb185074b80ad212ae6f2f
Mamad78 <mamadi78.94@gmail.com>
2026-02-24 23:38:38 +0330

    graphQL & ApoloClient added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const QUERY = gql`
+  query {
+    authors {
+      name
+    }
+  }
+`;
 
 function App() {
-
+  const { loading, data } = useQuery(QUERY);
+  console.log(data);
   return (
     <>
       <h3>GraphQl</h3>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://us-west-2.cdn.hygraph.com/content/cmk0wn9yf03gp07w51vfxs90s/master",
+  }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>,
+);
